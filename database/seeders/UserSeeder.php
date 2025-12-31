cd1c4bf20fa33f4ee714dd1bdefc108c77dbf970
Yas3tech <eddouksy@gmail.com>
2025-12-31 22:41:29 +0100

    Add admin middleware, migration, and user seeder
    Introduces AdminMiddleware for route protection, registers it in the middleware aliases, and adds a migration to include an is_admin field on the users table. Also refactors DatabaseSeeder to use a new UserSeeder, which seeds an admin user.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@ehb.be'],
+            [
+                'name' => 'admin',
+                'password' => Hash::make('Password!321'),
+                'is_admin' => true,
+            ]
+        );
+    }
+}
